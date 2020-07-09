0b858a03371ad6ee6237db653621eca49f2fbc73
Olga Patrakova <olgapatrakova@outlook.com>
2020-07-09 16:45:08 -0700

    made topic displayed in the bot emulator using prompts

--- bot/dialog/choose_topic_dialog.py
+++ bot/dialog/choose_topic_dialog.py
@@ -1,6 +1,7 @@
 from asgiref.sync import sync_to_async
 from botbuilder.core import MessageFactory
-from botbuilder.dialogs import ComponentDialog, WaterfallDialog, NumberPrompt, PromptValidatorContext, WaterfallStepContext, DialogTurnResult, PromptOptions
+from botbuilder.dialogs import ComponentDialog, WaterfallDialog, \
+    WaterfallStepContext, DialogTurnResult, PromptOptions, ChoicePrompt, Choice
 
 from bot.models import Deck
 
@@ -9,12 +10,21 @@ class ChooseTopicDialog(ComponentDialog):
     def __init__(self, dialog_id: str = None):
         super(ChooseTopicDialog, self).__init__(dialog_id or ChooseTopicDialog.__name__)
 
-        self.add_dialog(WaterfallDialog(WaterfallDialog.__name__, [self.selection_step,]))
+        self.add_dialog(WaterfallDialog(WaterfallDialog.__name__, [self.give_choice_step,]))
+        self.add_dialog(ChoicePrompt(ChoicePrompt.__name__))
 
-    async def selection_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
+        self.initial_dialog_id = WaterfallDialog.__name__
+
+    async def give_choice_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
         decks = await self.decks()
-        await step_context.context.send_activity(MessageFactory.text(f"I have these topics for you to learn: {decks}."))
-        return await step_context.end_dialog()
+
+        return await step_context.prompt(
+            ChoicePrompt.__name__,
+            PromptOptions(
+                prompt=MessageFactory.text("I have these topics for you to learn. Please choose one."),
+                choices=[Choice(x.title) for x in decks],
+            ),
+        )
 
     @sync_to_async
     def decks(self):
