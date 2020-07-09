be5548a84f3602a8585b3524f0ca65eadc7e9e60
Olga Patrakova <olgapatrakova@outlook.com>
2020-07-09 11:44:38 -0700

    created initial models

--- bot/dialog/choose_topic_dialog.py
+++ bot/dialog/choose_topic_dialog.py
@@ -8,14 +8,6 @@ class ChooseTopicDialog(ComponentDialog):
         self.add_dialog(WaterfallDialog(WaterfallDialog.__name__, [self.selection_step,]))
 
     async def selection_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
-        # WaterfallStep always finishes with the end of the Waterfall or with another dialog;
-        # here it is a Prompt Dialog. Running a prompt here means the next WaterfallStep will
-        # be run when the users response is received.
-
-        # if step_context.options is None:
-        #     raise Exception("internal error, number not passed in")
-
-        # step_context.values["number"] = step_context.options
 
         await step_context.context.send_activity(MessageFactory.text("I have 5 topics for you to learn."))
         return await step_context.end_dialog()
--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.db.models import DateTimeField, IntegerField
 
-# Create your models here.
+class Deck(models.Model):
+    title = models.CharField()
+
+    def __unicode__(self):
+        return self.title
+
+class Card(models.Model):
+    deck = models.ForeignKey('Deck', on_delete=models.CASCADE, related_name='deck')
+    front = models.CharField()
+    back = models.CharField()
+
+    def __unicode__(self):
+        return self.front
+
+class Question(models.Model):
+    card = models.ForeignKey('Card', on_delete=models.CASCADE, related_name='card')
+    text = models.TextField()
+    def __unicode__(self):
+        return self.text
+
+class Answer(models.Model):
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='question')
+    text = models.TextField()
+    type = models.CharField()
+
+    def __unicode__(self):
+        return self.text
+
+class User(models.Model):
+    name = models.CharField()
+    last_interaction_time = DateTimeField()
+
+    def __unicode__(self):
+        return self.name
+
+class LearningMatrix(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='user')
+    deck = models.ForeignKey('Deck', on_delete=models.CASCADE, related_name='deck')
+    card = models.CharField()
+    last_shown = DateTimeField()
+    show_after = DateTimeField()
+    show_count = IntegerField()
+    easy_count = IntegerField()
+    hard_count = IntegerField()
+
+    def __unicode__(self):
+        return self.card
