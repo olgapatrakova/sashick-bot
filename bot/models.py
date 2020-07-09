419bd271dea451116d3f87bf6ee21dff7a7c1011
Olga Patrakova <olgapatrakova@outlook.com>
2020-07-09 14:16:33 -0700

    fixed models, added more details

--- bot/models.py
+++ bot/models.py
@@ -8,43 +8,61 @@ class Deck(models.Model):
         return self.title
 
 class Card(models.Model):
-    deck = models.ForeignKey('Deck', on_delete=models.CASCADE, related_name='deck')
-    front = models.CharField()
-    back = models.CharField()
+    deck = models.ForeignKey('Deck', on_delete=models.SET_NULL, related_name='cards', blank=True, null=True)
+    front = models.TextField()
+    back = models.TextField()
 
     def __unicode__(self):
         return self.front
 
 class Question(models.Model):
-    card = models.ForeignKey('Card', on_delete=models.CASCADE, related_name='card')
+    card = models.ForeignKey('Card', on_delete=models.CASCADE, related_name='questions')
     text = models.TextField()
+    BUTTON = 'BTN'
+    TEXT = 'TXT'
+    TYPE_CHOICES = [
+        (BUTTON, 'Button'),
+        (TEXT, 'Text'),
+    ]
+    type = models.CharField(
+        max_length=3,
+        choices=TYPE_CHOICES,
+        default=TEXT,
+    )
     def __unicode__(self):
         return self.text
 
 class Answer(models.Model):
-    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='question')
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='answers')
+    correct = models.NullBooleanField(blank=True)
     text = models.TextField()
-    type = models.CharField()
 
     def __unicode__(self):
         return self.text
 
 class User(models.Model):
-    name = models.CharField()
-    last_interaction_time = DateTimeField()
+    user_id = models.CharField(primary_key=True)
+    last_interaction_time = models.DateTimeField(auto_now=True, auto_now_add=True)
 
     def __unicode__(self):
-        return self.name
+        return self.user_id
 
 class LearningMatrix(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='user')
-    deck = models.ForeignKey('Deck', on_delete=models.CASCADE, related_name='deck')
-    card = models.CharField()
+    card = models.ForeignKey('Card', on_delete=models.CASCADE, related_name='card')
+    deck_title = models.CharField()
     last_shown = DateTimeField()
     show_after = DateTimeField()
     show_count = IntegerField()
     easy_count = IntegerField()
     hard_count = IntegerField()
 
+    class Meta:
+        unique_together = [['user', 'card']]
+
+        indexes = [
+            models.Index(fields=['user', 'card']),
+        ]
+
     def __unicode__(self):
-        return self.card
+        return f"{self.user} {self.card}"
