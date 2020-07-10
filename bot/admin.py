e199b26b2ea20871ba3ca75d5198401b0e0549e7
Olga Patrakova <olgapatrakova@outlook.com>
2020-07-10 14:20:50 -0700

    added CardAdmin to show columns Deck, Front, Back in Card admin, added filtering cards by a deck

--- bot/admin.py
+++ bot/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 from .models import Deck, Card, Question, Answer, User, LearningMatrix
 
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('front', 'back', 'deck')
+    list_filter = ['deck']
+
 admin.site.register(Deck)
-admin.site.register(Card)
+admin.site.register(Card, CardAdmin)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(User)
 admin.site.register(LearningMatrix)
+
