97694027d2c3ecc84f6e59ce2ab405df4b7536b9
Olga Patrakova <olgapatrakova@outlook.com>
2020-07-09 17:32:17 -0700

    confirmation step for choose topic implemented

--- bot/dialog/choose_topic_dialog.py
+++ bot/dialog/choose_topic_dialog.py
@@ -1,7 +1,7 @@
 from asgiref.sync import sync_to_async
 from botbuilder.core import MessageFactory
 from botbuilder.dialogs import ComponentDialog, WaterfallDialog, \
-    WaterfallStepContext, DialogTurnResult, PromptOptions, ChoicePrompt, Choice
+    WaterfallStepContext, DialogTurnResult, PromptOptions, ChoicePrompt, Choice, ConfirmPrompt
 
 from bot.models import Deck
 
@@ -10,9 +10,9 @@ class ChooseTopicDialog(ComponentDialog):
     def __init__(self, dialog_id: str = None):
         super(ChooseTopicDialog, self).__init__(dialog_id or ChooseTopicDialog.__name__)
 
-        self.add_dialog(WaterfallDialog(WaterfallDialog.__name__, [self.give_choice_step,]))
+        self.add_dialog(WaterfallDialog(WaterfallDialog.__name__, [self.give_choice_step, self.confirm_choice_step, self.choose_again_step,]))
         self.add_dialog(ChoicePrompt(ChoicePrompt.__name__))
-
+        self.add_dialog(ConfirmPrompt(ConfirmPrompt.__name__))
         self.initial_dialog_id = WaterfallDialog.__name__
 
     async def give_choice_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
@@ -21,10 +21,28 @@ class ChooseTopicDialog(ComponentDialog):
         return await step_context.prompt(
             ChoicePrompt.__name__,
             PromptOptions(
-                prompt=MessageFactory.text("I have these topics for you to learn. Please choose one."),
+                prompt=MessageFactory.text("Please choose one topic to learn."),
                 choices=[Choice(x.title) for x in decks],
             ),
         )
+
+    async def confirm_choice_step(
+            self, step_context: WaterfallStepContext
+    ) -> DialogTurnResult:
+        step_context.values['deck'] = step_context.result
+
+        # WaterfallStep always finishes with the end of the Waterfall or
+        # with another dialog; here it is a Prompt Dialog.
+        return await step_context.prompt(
+            ConfirmPrompt.__name__,
+            PromptOptions(prompt=MessageFactory.text(f"You are going to learn {step_context.result.value}. Right?")),
+        )
+
+    async def choose_again_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
+        if step_context.result:
+            return await step_context.end_dialog(True)
+        else:
+            return await step_context.replace_dialog(ChooseTopicDialog.__name__)
 
     @sync_to_async
     def decks(self):
