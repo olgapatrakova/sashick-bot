8df0ba03a0f84425e59544afe1448a7a4e1b298a
Olga Patrakova <olgapatrakova@outlook.com>
2020-07-20 20:22:53 -0700

    implemented initial pro-active messaging functionality

--- bot/activity_handler.py
+++ bot/activity_handler.py
@@ -2,12 +2,12 @@ from datetime import datetime
 from asgiref.sync import sync_to_async
 from botbuilder.core import ActivityHandler, ConversationState, TurnContext, UserState, MessageFactory
 from botbuilder.dialogs import Dialog
-from botbuilder.schema import Attachment, Activity, ActivityTypes
+from botbuilder.schema import Attachment, Activity, ActivityTypes, ConversationReference
 
 from bot.dialog.helper import DialogHelper
 from bot.models import User
 from logging import getLogger
-
+from typing import Dict
 logger = getLogger(__name__)
 
 
@@ -21,7 +21,7 @@ class DialogBot(ActivityHandler):
     """
 
     def __init__(
-            self, conversation_state: ConversationState, user_state: UserState, dialog: Dialog,
+            self, conversation_state: ConversationState, user_state: UserState, dialog: Dialog, conversation_references: Dict[str, ConversationReference]
     ):
         if conversation_state is None:
             raise TypeError("[DialogBot]: Missing parameter. conversation_state is required but None was given")
@@ -34,6 +34,7 @@ class DialogBot(ActivityHandler):
         self.welcomed = conversation_state.create_property("welcomed")
         self.user_state = user_state
         self.dialog = dialog
+        self.conversation_references = conversation_references
 
     async def on_turn(self, turn_context: TurnContext):
         await super().on_turn(turn_context)
@@ -43,6 +44,7 @@ class DialogBot(ActivityHandler):
         await self.user_state.save_changes(turn_context)
 
     async def on_conversation_update_activity(self, turn_context: TurnContext):
+        self._add_conversation_reference(turn_context.activity)
         user_id = turn_context.activity.members_added[1].id
         await self.get_or_create_user(user_id)
         already_welcomed = await self.welcomed.get(turn_context, default_value_or_factory=False)
@@ -57,9 +59,22 @@ class DialogBot(ActivityHandler):
         )
 
     async def on_message_activity(self, turn_context: TurnContext):
+        self._add_conversation_reference(turn_context.activity)
         await DialogHelper.run_dialog(
             self.dialog, turn_context, self.conversation_state.create_property("DialogState"),
         )
+
+    def _add_conversation_reference(self, activity: Activity):
+        """
+        This populates the shared Dictionary that holds conversation references. In this sample,
+        this dictionary is used to send a message to members when /api/notify is hit.
+        :param activity:
+        :return:
+        """
+        conversation_reference = TurnContext.get_conversation_reference(activity)
+        self.conversation_references[
+            conversation_reference.user.id
+        ] = conversation_reference
 
     @sync_to_async
     def get_or_create_user(self, user_id):
--- bot/bot.py
+++ bot/bot.py
@@ -2,11 +2,17 @@ import os
 import sys
 import traceback
 from datetime import datetime
-from botbuilder.schema import Activity, ActivityTypes
+from typing import Dict
+from http import HTTPStatus
+
+from botbuilder.core.integration import aiohttp_error_middleware
+from botbuilder.schema import Activity, ActivityTypes, ConversationReference
 
 from bot.activity_handler import DialogBot
 from bot.dialog.main_dialog import MainDialog
 from dotenv import load_dotenv
+from aiohttp import web
+from aiohttp.web import Request, Response, json_response
 
 from bot.state import CONVERSATION_STATE, USER_STATE
 
@@ -20,6 +26,7 @@ from botbuilder.core import (
     TurnContext,
     UserState,
 )
+
 
 class DefaultConfig:
     """ Bot Configuration """
@@ -42,7 +49,7 @@ async def on_error(context: TurnContext, error: Exception):
     # This check writes out errors to console log
     # NOTE: In production environment, you should consider logging this to Azure
     #       application insights.
-    print(f"\n [on_turn_error]: { error }", file=sys.stderr)
+    print(f"\n [on_turn_error]: {error}", file=sys.stderr)
     traceback.print_exc()
 
     # Send a message to the user
@@ -71,8 +78,56 @@ async def on_error(context: TurnContext, error: Exception):
 # In this case, we want an unbound method, so MethodType is not needed.
 ADAPTER.on_turn_error = on_error
 
-
+# Create a shared dictionary.  The Bot will add conversation references when users
+# join the conversation and send messages.
+CONVERSATION_REFERENCES: Dict[str, ConversationReference] = dict()
 
 # create main dialog and bot
 DIALOG = MainDialog(CONVERSATION_STATE, USER_STATE)
-BOT = DialogBot(CONVERSATION_STATE, USER_STATE, DIALOG)
+APP_ID = os.getenv("APP_ID")
+BOT = DialogBot(CONVERSATION_STATE, USER_STATE, DIALOG, CONVERSATION_REFERENCES)
+
+
+# Listen for incoming requests on /api/messages.
+async def messages(req: Request) -> Response:
+    # Main bot message handler.
+    if "application/json" in req.headers["Content-Type"]:
+        body = await req.json()
+    else:
+        return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
+
+    activity = Activity().deserialize(body)
+    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
+
+    response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+    if response:
+        return json_response(data=response.body, status=response.status)
+    return Response(status=HTTPStatus.OK)
+
+
+# Listen for requests on /api/notify, and send a messages to all conversation members.
+async def notify(req: Request) -> Response:  # pylint: disable=unused-argument
+    await _send_proactive_message()
+    return Response(status=HTTPStatus.OK, text="Proactive messages have been sent")
+
+
+# Send a message to all conversation members.
+# This uses the shared Dictionary that the Bot adds conversation references to.
+async def _send_proactive_message():
+    for conversation_reference in CONVERSATION_REFERENCES.values():
+        await ADAPTER.continue_conversation(
+            conversation_reference,
+            lambda turn_context: turn_context.send_activity("proactive hello"),
+            APP_ID,
+        )
+
+
+APP = web.Application(middlewares=[aiohttp_error_middleware])
+APP.router.add_post("/api/messages", messages)
+APP.router.add_get("/api/notify", notify)
+
+if __name__ == "__main__":
+    try:
+        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+    except Exception as error:
+        raise error
